72890daed5d843d142018958571ed057aa3feff7
HackerRank <hackerrank@example.com>
2019-02-25 01:07:37 +0000

    Saved at Sun Feb 24 2019 19:07:37 GMT-0600 (Central Standard Time)

--- src/components/Search.js
+++ src/components/Search.js
@@ -4,7 +4,7 @@ import Input from '@material-ui/core/Input'
 class Search extends Component {
     constructor(props){
         super(props)
-        thi
+        this.stat
         
     }
     render() {
